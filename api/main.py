00ca9a92a78dfb8d82327a9b1aa9673b4744e6ae
Nargiza Imasheva <imnaur@bk.ru>
2026-08-12 09:58:12 +0200

    project architecture

---
+++ api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from typing import Literal
+#
+app = FastAPI()
+#
+# @app.get("/")
+# def root():
+#     return {'Hello': 'World!'}
+#
+# #@app.get('/')
+# # async def read_results():
+# #     results = await some_library()
+# #     return results
